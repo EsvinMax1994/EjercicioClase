366644ccafc924d4c1687d0eec78cbcebdd51764
EsvinMax1994 <maxdani94@outlook.com>
2024-09-19 21:06:41 -0600

    Se agrego Practica de la teoria vista en clase, ejercicio Tienda Electronica

---
+++ POO/src/main/java/tiendaElectronica/ProductoElectronico.java
@@ -0,0 +1,35 @@
+
+package tiendaElectronica;
+
+
+public abstract class ProductoElectronico {
+    double precio;
+    String marca;
+    String modelo;
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    
+}
---
+++ POO/src/main/java/tiendaElectronica/Tableta.java
@@ -0,0 +1,25 @@
+package tiendaElectronica;
+
+
+public class Tableta extends ProductoElectronico{
+    double tamanoPantalla;
+    String resolucionPantalla;
+
+    public double getTamanoPantalla() {
+        return tamanoPantalla;
+    }
+
+    public void setTamanoPantalla(double tamanoPantalla) {
+        this.tamanoPantalla = tamanoPantalla;
+    }
+
+    public String getResolucionPantalla() {
+        return resolucionPantalla;
+    }
+
+    public void setResolucionPantalla(String resolucionPantalla) {
+        this.resolucionPantalla = resolucionPantalla;
+    }
+    
+    
+}
